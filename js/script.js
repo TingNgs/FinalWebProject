09dcca76a2738ae096f40fa32a39d3dbba124640
ngSgn <t105590049@ntut.org.tw>
2018-06-17 01:48:02 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    //$("#footer").load("footer.html");
+    $("#header").load("header.html");
+});
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
