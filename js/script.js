bdc8daa797f5eb6fc1f10d5d05bcc7a99a5265c0
ngSgn <t105590049@ntut.org.tw>
2018-06-18 12:51:01 +0800

    test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,71 @@
-$(function() {
-    //$("#footer").load("footer.html");
+$(function () {
+	//$("#footer").load("footer.html");
 	$('header').load('header.html');
-	
+
 });
-$(document).ready(function(){
-	$(document).on("click", "#nav-icon", function() {
+$(document).ready(function () {
+	var config = {
+		apiKey: "AIzaSyAgTbBfhw6qEJ_2UOMRKFDqSqX_oMW_0kc",
+		authDomain: "webfinalproject-9ddcc.firebaseapp.com",
+		databaseURL: "https://webfinalproject-9ddcc.firebaseio.com",
+		projectId: "webfinalproject-9ddcc",
+		storageBucket: "webfinalproject-9ddcc.appspot.com",
+		messagingSenderId: "462597688502"
+	  };
+	  firebase.initializeApp(config);
+
+	//var dbRef = firebase.database().ref.child('object');
+	//var ref = new Firebase("https://webfinalproject-9ddcc.firebaseio.com/");
+	//console.log(dbRef);
+
+	$(document).on("click", "#signUp-submit", function (e) {
+		const email = $('#email').val();
+		const pass = $('#password').val();
+		//const auth = firebase.auth();
+		//const promise = firebase.auth().createUserWithEmailAndPassword(email, pass);
+		//promise.catch(function (e) {
+		//	console.log(e.message);
+		//});
+		firebase.auth().createUserWithEmailAndPassword(email, pass)
+			.catch(function (error) {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				if (errorCode == 'auth/weak-password') {
+					alert('The password is too weak.');
+				} else {
+					alert(errorMessage);
+				}
+				console.log(error);
+			});
+	});
+	$(document).on("click", "#login-submit", function (e) {
+		const email = $('#email').val();
+		const pass = $('#password').val();
+		//const auth = firebase.auth();
+		//const promise = firebase.auth().createUserWithEmailAndPassword(email, pass);
+		//promise.catch(function (e) {
+		//	console.log(e.message);
+		//});
+		firebase.auth().signInWithEmailAndPassword(email, pass)
+			.catch(function (error) {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				if (errorCode == 'auth/weak-password') {
+					alert('The password is too weak.');
+				} else {
+					alert(errorMessage);
+				}
+				console.log(error);
+			});
+	});
+	//const auth = firebase.auth();
+	//auth.signInWithEmailAndPassword(email, pass);
+	//auth.createUserWithEmailAndPassword(email, pass)
+	//auth.onAuthStateChanged(firebaseUser => {});
+	$(document).on("click", "#nav-icon", function () {
 		$("#nav-icon").toggleClass('open');
+
 	});
 });
